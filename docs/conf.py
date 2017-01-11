b5eb3a4ee7a23154d9680c919b765dedd4e2fd7f
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2017-01-10 16:36:49 -0800

    Corrected copyright name

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sympl'
-copyright = u"2016, Rodrigo Caballero"
+copyright = u"2016, Jeremy McGibbon"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
